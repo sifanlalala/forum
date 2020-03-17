c4449e60364e79bf55f51d2e0a88122b80780da6
sifanlalala <yansifanaaa@126.com>
2020-03-17 16:18:32 +0800

    实现回复功能-不刷新页面登录

--- src/main/resources/static/js/forum.js
+++ src/main/resources/static/js/forum.js
@@ -13,9 +13,16 @@ function post() {
             if (response.code == 200){
                 $("#comment_section").hide();
             }else{
-                alert(response.message);
+                if (response.code == 2003){
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.be207b81d5e1e4e4&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                }else{
+                    alert(response.message);
+                }
             }
-            console.log(response)
         },
         dataType: "json",
         contentType:"application/json"
